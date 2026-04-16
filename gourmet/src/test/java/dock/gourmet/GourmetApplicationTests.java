c4ec65dbe38c78714b4cddd5750252f5c1d076bd
JonatanAN12 <jonatan25sanchez@gmail.com>
2026-04-16 10:25:40 -0500

    add new view history of cuts

---
+++ gourmet/src/test/java/dock/gourmet/GourmetApplicationTests.java
@@ -0,0 +1,13 @@
+package dock.gourmet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GourmetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
